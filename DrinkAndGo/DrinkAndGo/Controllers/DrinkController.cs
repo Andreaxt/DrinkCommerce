e4de080a0d8a94e9c49eaf85fb3254e009be910c
Andrea <20010912@studenti.uniupo.it>
2017-11-29 15:52:29 +0100

    aggiunto il progetto

---
+++ DrinkAndGo/DrinkAndGo/Controllers/DrinkController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DrinkAndGo.Data.Interfaces;
+using DrinkAndGo.ViewModels;
+
+namespace DrinkAndGo.Data.Controllers
+{
+    public class DrinkController : Controller
+    {
+        private readonly IDrinkRepository _drinkRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public DrinkController(IDrinkRepository drinkRepository, ICategoryRepository categoryRepository)
+        {
+            _drinkRepository = drinkRepository;
+            _categoryRepository = categoryRepository;
+        }
+        public IActionResult List()
+        {
+            ViewBag.name = "DotNet, who?";
+            var drinks = _drinkRepository.Drinks;
+
+            var vm = new DrinkListViewModel();
+            vm.Drinks = drinks;
+            vm.CurrentCategory = "Drink Category";
+            return View(vm);
+        }
+    }
+}
---
+++ DrinkAndGo/DrinkAndGo/Startup.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using DrinkAndGo.Data.Interfaces;
+using DrinkAndGo.Data.Mocks;
+using Microsoft.Extensions.Configuration;
+using DrinkAndGo.Data;
+using Microsoft.EntityFrameworkCore;
+using DrinkAndGo.Data.Repositories;
+
+namespace DrinkAndGo
+{
+    
+
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        private IConfigurationRoot _configurationRoot;
+
+        public Startup(IHostingEnvironment hostingEnviroment)
+        {
+            _configurationRoot = new ConfigurationBuilder().SetBasePath(hostingEnviroment.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(_configurationRoot.GetConnectionString("DefaultConnection")));
+
+
+
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<IDrinkRepository, DrinkRepository>();
+
+            services.AddMvc();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
+            DbInitializer.Seed(app);
+
+        }
+    }
+}
